25ce149ef865492bf1f871f7c967b0f39e28fb81
Iuliia Plekhanova <iuliia_plekhanova@epam.com>
2023-03-05 15:51:39 +0400

    feat: add deep count of files in folder

--- module_4/task_1/countfilesdeep.sh
+++ module_4/task_1/countfilesdeep.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+count() {
+  echo "$(find $1 -type f | wc -l)"
+}
+
+if (($#==0))
+then
+  echo "Please provide the directory"
+fi
+
+for directory in "$@"
+do
+  if [ ! -d "$directory" ]
+  then
+    echo "**" Skipping $directory
+    continue
+  fi
+
+
+  res=$(count "$directory")
+  echo "Directory $directory and it's subdirectories contain $res files"
+done
+
 

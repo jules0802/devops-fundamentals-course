53b01a00de6232053d20d8b9d9c2bb43fa5cd338
Iuliia Plekhanova <iuliia_plekhanova@epam.com>
2023-03-12 00:05:02 +0400

    feat: add help command

--- module_4/task_2/scripts/db.sh
+++ module_4/task_2/scripts/db.sh
@@ -38,6 +38,25 @@ addUser() {
   fi
 }
 
+showInfo() {
+  echo "db.sh helps manipulating users DB"
+  echo "Syntax: db.sh [command] [-parameter]"
+  echo "Following commands are available:"
+  echo "add    Adds a new line to the users.db. The script must prompt a user to type the username of a new entity.
+After entering the username, the user must be prompted to type a role."
+  echo "backup Creates a new file, named %date%-users.db.backup which is a copy of
+current users.db"
+  echo "find   Prompts user to type a username, then prints username and role if such
+exists in users.db. If there is no user with selected username, script must print:
+“User not found”. If there is more than one user with such username, print all
+found entries."
+  echo "list   Prints contents of users.db in format: N. username, role
+where N – a line number of an actual record
+Accepts an additional optional parameter inverse which allows to get
+result in an opposite order – from bottom to top"
+ echo "help    Prints instructions on how to use this script with a description of all available commands"
+}
+
 
 
 case "$command" in
@@ -45,6 +64,6 @@ case "$command" in
   backup)  ;;
   restore) ;;
   find) ;;
-  "" | help) ;;
+  "" | help) showInfo ;;
   list) ;;
 esac
